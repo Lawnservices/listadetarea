dd6c2a73b69d3dd503049b2df4875f44ff98f22a
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-06-23 18:43:38 -0400

    fix

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 const PORT = 3000;
 
@@ -8,6 +9,17 @@ app.use(express.json());
 // Sirve tu carpeta public (HTML, CSS, JS)
 app.use(express.static('public'));
 
+
+
+// ruta limpia
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/login.html"));
+});
+
+// otra página ejemplo
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/registro.html"));
+});
 // Servidor funcionando
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
