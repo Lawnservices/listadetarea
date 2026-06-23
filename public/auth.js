1f1e24deb0b877c552e86643c375a6cca80b7a69
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-06-23 19:28:40 -0400

    fix

--- public/auth.js
+++ public/auth.js
@@ -42,7 +42,7 @@ async function login() {
   }
 
   localStorage.setItem("token", data.token);
-  window.location.href = "tareas";
+  window.location.href = "tareas.html";
 }
 
 // LOGOUT (solo borra token)
